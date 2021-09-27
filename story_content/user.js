a888154e7c651de916636695b09bbb84163717a9
Giang <ngangiangtran91@gmail.com>
2021-09-27 16:11:38 +0700

    s

---
+++ story_content/user.js
@@ -0,0 +1,137 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xjelT9Xd0c":
+        Script1();
+        break;
+      case "6MZ9Uat6g1a":
+        Script2();
+        break;
+      case "6et5ei17lw9":
+        Script3();
+        break;
+      case "5tzAfyUFr7r":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+   var $ROOTPATHOFWPAINT="../";
+  if(document.getElementById("includeJavascriptWpaint")!=undefined)
+  {       
+    showHidenWpaint("board");
+  }
+  else    
+  {                 
+    var fileref=document.createElement('script');
+    fileref.setAttribute("type","text/javascript");
+    fileref.setAttribute("id","includeJavascriptWpaint");    
+    fileref.setAttribute("name","board");    
+    fileref.setAttribute("src",$ROOTPATHOFWPAINT+"wPaint/includeJavascript.js");
+    if (typeof fileref!="undefined")
+    {                   
+      var tagDiv= document.getElementById("div_cnt_js_css");
+      if(tagDiv==undefined)
+      {
+       tagDiv= document.createElement('div');
+       tagDiv.setAttribute("id","div_cnt_js_css");              
+      }
+      else{tagDiv.innerHTML="";}
+      tagDiv.appendChild(fileref);
+
+      document.getElementsByTagName("body")[0].appendChild(tagDiv);                              
+    }
+  }  
+}
+
+function Script2()
+{
+  var $ROOTPATHOFWPAINT="../";
+ if(document.getElementById("includeJavascriptWpaint")!=undefined)
+  {       
+    showHidenWpaint("pen");
+  }
+  else    
+  {                 
+    var fileref=document.createElement('script');
+    fileref.setAttribute("type","text/javascript");
+    fileref.setAttribute("id","includeJavascriptWpaint");    
+    fileref.setAttribute("name","pen");    
+    fileref.setAttribute("src",$ROOTPATHOFWPAINT+"wPaint/includeJavascript.js");
+    if (typeof fileref!="undefined")
+    {           
+    var tagDiv= document.getElementById("div_cnt_js_css");
+      if(tagDiv==undefined)
+      {
+       tagDiv= document.createElement('div');
+       tagDiv.setAttribute("id","div_cnt_js_css");              
+      }
+      else{tagDiv.innerHTML="";}
+      tagDiv.appendChild(fileref);      
+      document.getElementsByTagName("body")[0].appendChild(tagDiv);                              
+    }
+  }  
+}
+
+function Script3()
+{
+   var $ROOTPATHOFWPAINT="../";
+  if(document.getElementById("includeJavascriptWpaint")!=undefined)
+  {       
+    showHidenWpaint("board");
+  }
+  else    
+  {                 
+    var fileref=document.createElement('script');
+    fileref.setAttribute("type","text/javascript");
+    fileref.setAttribute("id","includeJavascriptWpaint");    
+    fileref.setAttribute("name","board");    
+    fileref.setAttribute("src",$ROOTPATHOFWPAINT+"wPaint/includeJavascript.js");
+    if (typeof fileref!="undefined")
+    {                   
+      var tagDiv= document.getElementById("div_cnt_js_css");
+      if(tagDiv==undefined)
+      {
+       tagDiv= document.createElement('div');
+       tagDiv.setAttribute("id","div_cnt_js_css");              
+      }
+      else{tagDiv.innerHTML="";}
+      tagDiv.appendChild(fileref);
+
+      document.getElementsByTagName("body")[0].appendChild(tagDiv);                              
+    }
+  }  
+}
+
+function Script4()
+{
+  var $ROOTPATHOFWPAINT="../";
+ if(document.getElementById("includeJavascriptWpaint")!=undefined)
+  {       
+    showHidenWpaint("pen");
+  }
+  else    
+  {                 
+    var fileref=document.createElement('script');
+    fileref.setAttribute("type","text/javascript");
+    fileref.setAttribute("id","includeJavascriptWpaint");    
+    fileref.setAttribute("name","pen");    
+    fileref.setAttribute("src",$ROOTPATHOFWPAINT+"wPaint/includeJavascript.js");
+    if (typeof fileref!="undefined")
+    {           
+    var tagDiv= document.getElementById("div_cnt_js_css");
+      if(tagDiv==undefined)
+      {
+       tagDiv= document.createElement('div');
+       tagDiv.setAttribute("id","div_cnt_js_css");              
+      }
+      else{tagDiv.innerHTML="";}
+      tagDiv.appendChild(fileref);      
+      document.getElementsByTagName("body")[0].appendChild(tagDiv);                              
+    }
+  }  
+}
+
